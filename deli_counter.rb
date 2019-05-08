3cd956f3b48c2423d783852f5471738236adf59b
Danielle Martinoli <daniechris5@gmail.com>
2019-05-08 03:03:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,21 @@
+require "pry"
+
 katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-    return "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def line(katz_deli)
-  if line == 0
-    return "The line is currently empty."
+  word = "The line is currently:"
+  if katz_deli.length == 0
+    puts "The line is currently empty."
   else
-    return
-          "The line is currently #{line}"
+    katz_deli.each do |name|
+     word = word + katz_deli.index(name)+1 +"#{name}"
+     #string interpolating and appending with index
+  end
 end
 
 def now_serving
