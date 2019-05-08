cd18eebc9b7f57a3e3f1d553f942ff4e2112fc9e
Danielle Martinoli <daniechris5@gmail.com>
2019-05-08 16:20:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,16 +8,25 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-  word = " The line is currently:"
+  current_line = "The line is currently:"
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-    word += "#{index+1}" "#{name}"
+    current_line += " #{index+1}." " #{name}"
     end
-    puts word
+    puts current_line
   end
 end
 
-def now_serving
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    name = katz_deli(0)
+    katz_deli.each_with_index do |name, index|
+    name += katz_deli.slice(0, 1);
+  end
+  puts "Currently serving" + name + '.'
+ end
 end
