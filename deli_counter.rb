bfa75f8383567a5ae7f3b170870649166d45e185
Danielle Martinoli <daniechris5@gmail.com>
2019-05-08 02:34:21 +0000

    Done.

---
+++ deli_counter.rb
@@ -0,0 +1,17 @@
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+    return "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def line(katz_deli)
+  if line == 0
+    return "The line is currently empty."
+  else
+    return
+          "The line is currently #{line}"
+end
+
+def now_serving
+end
